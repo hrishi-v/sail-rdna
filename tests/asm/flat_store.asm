de93f7ed375123755e829f5155d9b40bbb502596
hv122 <hrishikesh.venkatesh22@imperial.ac.uk>
2026-03-30 17:44:26 +0100

    fix: asm changes for uninitialised registers

--- tests/asm/flat_store.asm
+++ tests/asm/flat_store.asm
@@ -3,5 +3,7 @@ v_mov_b32 v0, 0x00002008
 v_mov_b32 v1, 0x00000000
 v_mov_b32 v2, 0xCAFEBABE
 flat_store_b32 v[0:1], v2
+s_waitcnt vmcnt(0)
+flat_load_b32 v3, v[0:1]
+s_waitcnt vmcnt(0)
 s_endpgm
-
--- tests/asm/v_add.asm
+++ tests/asm/v_add.asm
@@ -1,3 +1,4 @@
-s_mov_b32 exec_lo, -1 
+s_mov_b32 exec_lo, -1
+v_mov_b32 v1, 0
 v_add_nc_u32_e32 v0, 42, v1
 s_endpgm
